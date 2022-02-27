9e3518de4b51f616a6638dd27725256531c3ed84
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-27 16:48:52 -0700

    added a couple more helper functions

--- helpers.js
+++ helpers.js
@@ -1,6 +1,3 @@
-
-
-
 function emailChecker(email, users) {
   for (let address in users) {
     if (email === users[address].email) {
@@ -10,7 +7,23 @@ function emailChecker(email, users) {
   return undefined;
 };
 
+function urlsForUser(userID, urlDatabase) {
+  let usersURLs = {};
+  for (let url in urlDatabase) {
+    if (userID === urlDatabase[url].userID) {
+      usersURLs[url] = urlDatabase[url]
+    }
+  }
+  return usersURLs;
+};
+
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x10000000).toString(36);
+ };
+
 
 module.exports = {
-  emailChecker
+  emailChecker,
+  urlsForUser,
+  generateRandomString
 }
